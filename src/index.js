e52f19e3c9555a628647372c04f258e6b82350f6
Gregory <taylor.gregory2@gmail.com>
2021-11-28 14:29:49 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -15,15 +15,26 @@ function loadDogs(){
 
 dogForm.addEventListener('submit', e => {
     e.preventDefault()
-    console.log(e)
+    // console.log(e.target.getChildren)
+    let updatedDogName = dogForm.children[0].value
+    let updatedDogBreed = dogForm.children[1].value
+    let updatedDogSex = dogForm.children[2].value
+    let dogId = dogForm.dataset.id
+
+    let updatedDogData  = {
+        "name": updatedDogName,
+        "breed": updatedDogBreed,
+        "sex": updatedDogSex
+      }
     fetch(`http://localhost:3000/dogs/${dogId}`,{
         method: 'PATCH',
         headers: {
             'Content-Type':'application/json',
             'accept':'application/json'
         },
-        body: JSON.stringify(dog)
+        body: JSON.stringify(updatedDogData)
     })
+    console.log(updatedDogData)
 })
 
 
@@ -33,8 +44,12 @@ function renderDog(dog){
     let dogBreed = document.createElement('td')
     let dogSex = document.createElement('td')
     let dogButton = document.createElement('button')
-    let dogElementArray = [dogName, dogBreed, dogSex, dogButton]
+    let hiddenField = document.createElement('td')
     let dogId = dog.id
+    hiddenField.hidden = true
+    hiddenField.innerHTML = dogId
+    let dogElementArray = [dogName, dogBreed, dogSex, dogButton, hiddenField]
+    
     dogRow.setAttribute('id',dog.id)
     dogName.innerHTML = dog.name
     dogSex.innerHTML = dog.sex
@@ -49,6 +64,8 @@ function renderDog(dog){
         dogForm.children[0].value = dog.name
         dogForm.children[1].value = dog.breed
         dogForm.children[2].value =dog.sex
+        dogForm.dataset.id = dog.id
+        
         
     })
     
